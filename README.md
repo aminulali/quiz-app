16defde00a3cb0963395abaa5f98bec2a45eabde
Ali <60712126+aminulali@users.noreply.github.com>
2020-07-14 23:44:49 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# quiz-app
 Quiz App Project
 
-Open: 'src' folder then 'components' folder.
+Technologies used: React
+
+About: A quiz app project which allows the user to select an option for each question. After the user has answered all 3 questions, the users score will be displayed.
+
+To view code, open folder -> 'src' then open files -> index.js
+To view code, open folder -> 'src' then open folder -> 'components' then open files -> QuestionBox.js --- Result.js
---
+++ build/precache-manifest.61b37b79c79e10dad50b6ce3c396ba29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d968a7efc1bcd002e89dfd0ce107dba3",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "64dbc55913abfabcc027",
+    "url": "/quiz-app/static/css/main.2ec71153.chunk.css"
+  },
+  {
+    "revision": "1538cf265ebce00dd566",
+    "url": "/quiz-app/static/js/2.2175e3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.2175e3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64dbc55913abfabcc027",
+    "url": "/quiz-app/static/js/main.27e6dfbf.chunk.js"
+  },
+  {
+    "revision": "b361294a09da10485cc7",
+    "url": "/quiz-app/static/js/runtime-main.bd5e6a93.js"
+  }
+]);
